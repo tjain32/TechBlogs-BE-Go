30467f8a2808c1f2459710c8033feedff526ae1b
Tanishqa <21tanishqa@gmail.com>
2024-01-19 21:09:25 +0530

    first commit

---
+++ api/controllers/base.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/tjain32/forum/api/middlewares"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"    //mysql database driver
+	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres database driver
+	"github.com/tjain32/forum/api/models"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *gin.Engine
+}
+
+var errList = make(map[string]string)
+
+func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+
+	var err error
+
+	if Dbdriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		server.DB, err = gorm.Open(Dbdriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			log.Fatal("This is the error connecting to postgres:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", Dbdriver)
+		}
+	} else {
+		fmt.Println("Unknown Driver")
+	}
+
+	//database migration
+	server.DB.Debug().AutoMigrate(
+		&models.User{},
+		&models.Post{},
+		&models.ResetPassword{},
+		&models.Like{},
+		&models.Comment{},
+	)
+	// gin.SetMode(gin.ReleaseMode)
+	server.Router = gin.Default()
+	server.Router.Use(middlewares.CORSMiddleware())
+
+	server.initializeRoutes()
+
+}
+
+func (server *Server) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
